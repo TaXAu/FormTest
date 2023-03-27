ea81c48a8cd3956419f2cdd0367f56951573480d
ReeInk <hi@ree.ink>
2023-03-27 15:53:21 +0800

    use element plus

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from 'virtual:generated-pages'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 
 import '@unocss/reset/tailwind.css'
@@ -13,4 +15,5 @@ const router = createRouter({
   routes,
 })
 app.use(router)
+app.use(ElementPlus)
 app.mount('#app')
